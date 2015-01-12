5d22e0b3db21e980189155503c58dd8fa00e7a39
Azlan And Syazwan <dev+azlan+syazwan@ezeratech.com>
2015-01-12 16:52:52 +0800

    initial commit

---
+++ app/models/project.rb
@@ -0,0 +1,19 @@
+class Project
+	attr_accessor :tasks
+
+	def initialize
+		@tasks = []
+	end
+
+	def done?
+		tasks.reject(&:complete?).empty?
+	end
+
+	def total_size
+		tasks.sum(&:size)
+	end
+
+	def remaining_size
+		tasks.reject(&:complete?).sum(&:size)
+	end	
+end
---
+++ app/models/task.rb
@@ -0,0 +1,17 @@
+class Task
+
+	attr_accessor :size, :completed
+
+	def initialize(options = {})
+		@completed = options[:completed]
+		@size = options[:size]
+	end
+
+	def mark_completed
+		@completed = true
+	end
+
+	def complete?
+		@completed
+  end
+end
